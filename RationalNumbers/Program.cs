3cda36702fd87bb3d2a388ccdea39a5e5c00fe6e
georgebreje <george.breje2@gmail.com>
2021-03-04 14:09:19 +0200

    add basic operations and simplification

--- RationalNumbers/Program.cs
+++ RationalNumbers/Program.cs
@@ -10,6 +10,15 @@ namespace RationalNumbers
     {
         static void Main(string[] args)
         {
+            Rational r1 = new Rational(13,3);
+            Rational r2 = new Rational(5, 5);
+            Rational sum = r1.Add(r2);
+            Console.WriteLine(sum);
+            sum.Simplify();
+            Console.WriteLine(sum);
+            Console.WriteLine(r1);
+            r1.Simplify();
+            Console.WriteLine(r1);
         }
     }
 }
---
+++ RationalNumbers/Rational.cs
@@ -0,0 +1,108 @@
+﻿namespace RationalNumbers
+{
+    class Rational
+    {
+        private int a;
+        private int b;
+        public Rational(int a, int b)
+        {
+            this.a = a;
+            this.b = b;
+        }
+        public void Simplify()
+        {
+            if (a % b != 0)
+            {
+                if (cmmdc(a, b) != 1)
+                {
+                    a = a / cmmdc(a, b);
+                    b = b / cmmdc(a, b);
+                }
+                else if (cmmdc(a,b) == 1)
+                {
+                    System.Console.WriteLine("can not be simplified");
+                }
+            }
+            else
+            {
+                a = a / b;
+                b = 1;
+            }
+        }
+        public Rational Add(Rational c)
+        {
+            Rational sum;
+            if (b == c.b)
+                sum = new Rational(a + c.a, b);
+            else
+                sum = new Rational(a * c.b + c.a * b, b * c.b);
+            return sum;
+        }
+        public Rational Substraction(Rational c)
+        {
+            Rational sub;
+            if (b == c.b)
+                sub = new Rational(a + c.a, b);
+            else
+                sub = new Rational(a * c.b + c.a * b, b * c.b);
+            return sub;
+        }
+        public Rational Multiply(Rational c)
+        {
+            Rational mult = new Rational(a*c.a,b*c.b);
+            return mult;
+        }
+        public Rational Division(Rational c)
+        {
+            Rational div = new Rational(a * c.b, b * c.a);
+            return div;
+        }
+        public static Rational operator + (Rational r1, Rational r2)
+        {
+            Rational sum;
+            if (r1.b == r2.b)
+                sum = new Rational(r1.a + r2.a, r1.b);
+            else
+                sum = new Rational(r1.a * r2.b + r2.a * r1.b, r1.b * r2.b);
+            return sum;
+        }
+        public static Rational operator - (Rational r1, Rational r2)
+        {
+            Rational sub;
+            if (r1.b == r2.b)
+                sub = new Rational(r1.a + r2.a, r1.b);
+            else
+                sub = new Rational(r1.a * r2.b + r2.a * r1.b, r1.b * r2.b);
+            return sub;
+        }
+        public static Rational operator * (Rational r1, Rational r2)
+        {
+            Rational mult = new Rational(r1.a * r2.a, r1.b * r2.b);
+            return mult;
+        }
+        public static Rational operator / (Rational r1, Rational r2)
+        {
+            Rational div = new Rational(r1.a * r2.b, r1.b * r2.a);
+            return div;
+        }
+        private int cmmdc(int a, int b)
+        {
+            while (a != b)
+            {
+                if (a > b)
+                    a -= b;
+                else
+                    b -= a;
+            }
+            return a;
+        }
+        public override string ToString()
+        {
+            if (b != 1)
+                return a.ToString() + "/" + b.ToString();
+            else if (b == 1)
+                return a.ToString();
+            return "";
+        }
+    }
+}
