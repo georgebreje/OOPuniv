3b580506c03d0f80d205baddd77899d81c146c68
georgebreje <george.breje2@gmail.com>
2021-04-22 13:10:46 +0300

    add DateTime parser

--- AgendaPersonala/Agenda.cs
+++ AgendaPersonala/Agenda.cs
@@ -10,13 +10,32 @@ namespace AgendaPersonala
     {
         private Person holder;
         private List<Activity> activityList;
+
         public Agenda(Person holder)
         {
             this.holder = holder;
         }
 
-        public void add(string description, DateTime start, DateTime finish)
+        private DateTime parseData(string[] inputDateHour)
         {
+            DateTime outData = new DateTime();
+            string[] formats = { "ddMMyyyy", "HHmm" };
+            bool ok = false;
+            foreach (var dataToParse in inputDateHour)
+            {
+                if (!DateTime.TryParseExact(dataToParse, formats, null,
+                System.Globalization.DateTimeStyles.AllowWhiteSpaces |
+                               System.Globalization.DateTimeStyles.AdjustToUniversal,
+                               out outData))
+                    throw new ArgumentException("Enter data in this format: ddMMyyyy HHmmss");
+            }
+            return outData;
+        }
+
+        public void add(string description, string[] inputStart, string[] inputFinish)
+        {
+            DateTime start = parseData(inputStart);
+            DateTime finish = parseData(inputFinish);
             Activity toAdd = new Activity(description, start, finish);
             foreach (Activity a in activityList)
                 if (DateTime.Compare(toAdd.getStart(), a.getStart()) == 0
