6a7d5ae7f0e537888fa6ea938556d7a27f1a9f78
georgebreje <george.breje2@gmail.com>
2021-05-10 14:11:43 +0300

    ball structure

---
+++ gameb/Ball.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace gameb
+{
+    class Ball
+    {
+        private int radius;
+        private int x;
+        private int y;
+        private int dirsense;
+        private int speed;
+        private Color color;
+        private string type;
+        private Random rnd = new Random();
+
+        public Ball()
+        {
+            this.radius = rnd.Next(5, 30);
+            this.x = rnd.Next(0, 1000);
+            this.y = rnd.Next(0, 600);
+            this.dirsense = rnd.Next(-180, 181);
+            this.speed = rnd.Next(1, 5);
+            this.color = Color.FromArgb(rnd.Next(0, 256), rnd.Next(0, 256), rnd.Next(0, 256));
+        }
+
+        public int Radius
+        {
+            get
+            {
+                return radius;
+            }
+        }
+        public int X
+        {
+            get
+            {
+                return x;
+            }
+        }
+        public int Y
+        {
+            get
+            {
+                return Y;
+            }
+        }
+        public int Dirsense
+        {
+            get
+            {
+                return dirsense;
+            }
+        }
+        public int Speed
+        {
+            get
+            {
+                return speed;
+            }
+        }
+        public Color Color
+        {
+            get
+            {
+                return color;
+            }
+        }
+        public string Type
+        {
+            get
+            {
+                return type;
+            }
+
+            set
+            {
+                type = value;
+            }
+        }
+    }
+}
--- gameb/Form1.cs
+++ gameb/Form1.cs
@@ -12,9 +12,28 @@ namespace gameb
 {
     public partial class Form1 : Form
     {
+        Bitmap bmp;
+        Graphics grp;
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void load()
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //bmp = new Bitmap(1200, 600);
+            //Ball myball = new Ball();
+            //grp = Graphics.FromImage(bmp);                                                 D E S E N A R E   U N   P U N C T              
+            //SolidBrush brush = new SolidBrush(myball.Color);
+            //grp.FillEllipse(brush, myball.x, myball.y, myball.radius, myball.radius);
+            //pictureBox1.Image = bmp;
+
+
         }
     }
 }
